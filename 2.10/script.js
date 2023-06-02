1e7d5bb196b43090106c980a674f69abd21f976a
Woodshox <1aleksandrpavlov@gmail.com>
2023-06-02 12:32:09 +0200

    2.10 learnJS

--- 2.10/script.js
+++ 2.10/script.js
@@ -1,3 +1,4 @@
+/*
 let result = prompt("Какое «официальное» название JavaScript?");
 
 if (result == "ECMAScript") {
@@ -5,4 +6,30 @@ if (result == "ECMAScript") {
 } else {
     alert("Не верно");
 }
+*/
+
+/*
+let result = prompt("Введите число любое число");
+
+if (result == 0) {
+    alert("0");
+} else if (result <0) {
+    alert("-1");
+} else {
+    alert(1)
+}
+*/
+
+/*
+let a = 1;
+let b = 2;
+let result = (a + b < 4) ? 'мало' : 'много';
+alert(result); 
+*/
+
+let login = prompt("введите логин")
+let message = (login == 'Сотрудник') ? 'Привет' :
+              (login == 'Директор') ? 'Здравствуйте' :
+              (login == '') ? 'Нет логина' : '';
+alert(message);
 
